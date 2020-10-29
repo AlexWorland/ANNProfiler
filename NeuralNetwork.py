575c9dfa27303dca3f23db4ee74c3415600ad754
alexworland <alexworland@gmail.com>
2020-10-29 11:05:21 -0700

    NeuralNetwork init commit

---
+++ NeuralNetwork.py
@@ -0,0 +1,7 @@
+"""
+Author: Alex Worland
+Date: 10/29/2020
+File: NeuralNetwork.py
+Description:
+    This file represents the neural network and all methods associated with retrieving information from it
+"""
