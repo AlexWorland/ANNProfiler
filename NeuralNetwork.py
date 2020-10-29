b9aabe2affa229014753ead8a4399abde267fa9d
alexworland <alexworland@gmail.com>
2020-10-29 12:09:12 -0700

    Implemented neural network construction based on user input

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,12 +6,17 @@ Description:
     This file represents the neural network and all methods associated with retrieving information from it
 """
 
+import tensorflow as tensorFlow
+
 
 class NeuralNetwork:
     # Default Values
     numHiddenLayers = 1
-    numNeuronsPerHLayer = 8
+    numNeuronsPerHLayer = 128
     trainingEpochs = 10
+    model = tensorFlow.keras.models.Sequential()
+    activationFunctionHidden = 'relu'
+    activationFunctionOutput = 'softmax'
 
     def __init__(self, numHiddenLayers, numNeuronsPerHLayer, trainingEpochs):
         self.numHiddenLayers = numHiddenLayers
@@ -26,4 +31,25 @@ class NeuralNetwork:
 
     def initFromTrainingEpochs(self, trainingEpochs):
         self.trainingEpochs = trainingEpochs
+
+    def createModel(self):
+        """
+        A function that creates the network from the user's parameters
+        :return:
+        """
+        # Input layer. Always the same: 28x28
+        self.model.add(tensorFlow.keras.layers.Flatten(input_shape=(28, 28, 1)))
+        # Add the desired amount of hidden layers, each with the given amount of neurons, each using the given
+        # activation function
+        for n in self.numHiddenLayers:
+            self.model.add(
+                tensorFlow.keras.layers.Dense(
+                    self.numNeuronsPerHLayer,
+                    activation=self.activationFunctionHidden
+                )
+            )
+        # Add the output layer. Size is always the same: 10 neurons each representing a base 10 digit
+        # User can pick the activation function, though TensorFlow recommends softmax
+        self.model.add(tensorFlow.keras.layers.Dense(10, activation=self.activationFunctionOutput))
+        return self.model
 
