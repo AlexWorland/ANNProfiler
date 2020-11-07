166f7cba0303d9805d8268036e186a8e326e3532
alexworland <alexworland@gmail.com>
2020-11-07 14:12:59 -0800

    Added try/catch to user data acquisition. Added user prompts to show what the model is doing. Added timing ability for one training set

--- ANNProfiler.py
+++ ANNProfiler.py
@@ -14,38 +14,65 @@ Description:
     own digit with their mouse and see the network work visually and see the prediction and accuracy displayed.
 """
 import tensorflow as tensorFlow
+import time
 from FinalProject.MNISTDataset import MNIST as MNISTData
-from FinalProject.NeuralNetwork import NeuralNetwork as NN
+from FinalProject.NeuralNetwork import NeuralNetwork as NeuralNetwork
 
 numHiddenLayers = 0
 numNeurons = 0
 numEpochs = 0
-activationFunctionMap = {0: "Rectified Linear (Recommended)",
+activFuncSelectionMap = {0: "Rectified Linear (Recommended For Hidden Layer)",
                          1: "Linear",
                          2: "Exponential Linear Unit",
                          3: "Exponential Activation",
                          4: "Sigmoid",
                          5: "Hard Sigmoid",
                          6: "Scaled Exponential Linear Unit",
-                         7: "Softmax",
+                         7: "Softmax (Recommended For Output Layer",
                          8: "Softplus",
                          9: "Softsign",
-                         10: "Swish"}
+                         10: "Swish",
+                         11: "Hyperbolic Tangent Activation Function"}
+activFuncKerasMap = {"Rectified Linear (Recommended For Hidden Layer)": 'relu',
+                     "Linear": 'linear',
+                     "Exponential Linear Unit": 'elu',
+                     "Exponential Activation": 'exponential',
+                     "Sigmoid": 'sigmoid',
+                     "Hard Sigmoid": 'hard_sigmoid',
+                     "Scaled Exponential Linear Unit": 'selu',
+                     "Softmax (Recommended For Output Layer)": 'softmax',
+                     "Softplus": 'softplus',
+                     "Softsign": 'softsign',
+                     "Swish": 'swish',
+                     "Hyperbolic Tangent Activation Function": 'tanh'}
 
 
 def main():
-    # Code for displaying usable devices
-    # from tensorflow.python.client import device_lib
-    # print(device_lib.list_local_devices())
+    # Display the welcome prompt
     welcomeUser()
-    userInfo = getUserInfo(activationFunctionMap)
+    # Get the number of hidden layers, neurons, and epochs from user
+    userInfo = getUserInfo(activFuncSelectionMap)
+    # Initialize the MNIST dataset
     mnistData = MNISTData()
+    # Create a new keras Sequential model
+    print("Creating Model...")
     model = tensorFlow.keras.models.Sequential()
+    # Initialize the model with the user data
+    print("Initializing Model...")
     model = intializeModel(userInfo, model)
+    # Compile the model
+    print("Compiling Model...")
     model = compileModel(model)
+    # Train the model with the MNIST dataset
+    print("Training Model...")
+    print("Beginning Training with", userInfo[2], "epochs:")
+    startTime = time.perf_counter()
     model = trainModel(model, mnistData)
-    # debug
-    # print(model)
+    endTime = time.perf_counter()
+    totalTrainingTime = endTime-startTime
+    print(totalTrainingTime, "seconds.")
+    # TODO: Display training metrics
+
 
 def welcomeUser():
     """
@@ -62,13 +89,21 @@ def getUserInfo(activationFunctionMap):
     :param activationFunctionMap:
     :return:
     """
-    numHiddenLayers = int(input("How many hidden layers would you like the network to have? : "))
-    numNeurons = int(input("How many neurons would you like those layers to have? : "))
-    numEpochs = int(input("How many epochs would you like to train the network? : "))
-    print("What activation function would you like to use for those neurons?")
-    for i in range(len(activationFunctionMap)):
-        print(activationFunctionMap.get(i), ":", i)
-    activationFunction = int(input("Please enter the activation function's number: "))
+    # TODO: Try/Catch for input mismatch
+    flag = True
+    while (flag):
+        try:
+            numHiddenLayers = int(input("How many hidden layers would you like the network to have? : "))
+            numNeurons = int(input("How many neurons would you like those layers to have? : "))
+            numEpochs = int(input("How many epochs would you like to train the network? : "))
+            print("What activation function would you like to use for those neurons?")
+            for i in range(len(activationFunctionMap)):
+                print(i, " : ", activationFunctionMap.get(i))
+            activationFunction = int(input("Please enter the activation function's number: "))
+            flag = False
+        except ValueError:
+            print("Please enter a valid number.")
+
     return [numHiddenLayers, numNeurons, numEpochs, activationFunction]
 
 
@@ -86,9 +121,9 @@ def intializeModel(userInfo, model):
     # TODO: Might give trouble. May need to reverse map. Will also need a map that maps full activation function
     #   name to shorthand. Could use activations.deserialize instead
 
-    activationFunction = activationFunctionMap.get(userInfo[3])
+    activationFunction = activFuncSelectionMap.get(userInfo[3])
     # Create new model based on user selections
-    model = NN(numHiddenLayers, numNeurons, numEpochs, activationFunction)
+    model = NeuralNetwork(numHiddenLayers, numNeurons, numEpochs, activationFunction)
     model.createModel()
     return model
 
@@ -117,10 +152,12 @@ def trainModel(model, mnistData):
 
 if __name__ == '__main__':
     # Required environment flags for macos
+    # TODO: Look into reason why
     import os
+
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
     # choice to use gpu or cpu for training
-    #with tensorFlow.device("/gpu:0"):
+    # with tensorFlow.device("/gpu:0"):
     #    main()
     # with tensorFlow.device("/cpu:0"):
     #     main()
