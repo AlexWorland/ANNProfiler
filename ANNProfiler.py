6b0447422d886b4a68e77e43647d7d480a18eb46
alexworland <alexworland@gmail.com>
2020-11-14 15:54:58 -0800

    Added measurement options for user to select. Changed how user information is prompted and saved. Added functions select for measurements.

--- ANNProfiler.py
+++ ANNProfiler.py
@@ -13,6 +13,7 @@ Description:
     The user can also interact with the network directly through the user input mode. The user can draw their
     own digit with their mouse and see the network work visually and see the prediction and accuracy displayed.
 """
+print("\nInitializing...")
 import tensorflow as tensorFlow
 from tensorflow.python.client import device_lib
 import time
@@ -46,35 +47,53 @@ activFuncKerasMap = {"Rectified Linear (Recommended For Hidden Layer(s))": 'relu
                      "Softsign": 'softsign',
                      "Swish": 'swish',
                      "Hyperbolic Tangent Activation Function": 'tanh'}
+measurements = {0: "Number of Neurons vs Training Time",
+                1: "Number of Hidden Layers vs Training Time",
+                2: "Number of Epochs vs Training Time",
+                3: "Number of Neurons vs Training Accuracy",
+                4: "Number of Hidden Layers vs Training Accuracy",
+                5: "Number of Epochs vs Training Accuracy"}
+patterns = {
+    0: "Linear",
+    1: "Exponential (Warning: This can grow very fast. Please choose a small initial number.)",
+    2: "Polynomial (Warning: This can grow very fast. Please choose a small initial number.)",
+    3: "Manual"
+    # TODO: Consider also: logarithmic, reciprocal, trigonometric, nth root
+}
 
 
 def main():
     # Display the welcome prompt
     welcomeUser()
-    # Get the number of hidden layers, neurons, and epochs from user
-    userInfo = getUserInfo(activFuncSelectionMap)
 
-    with tensorFlow.device(userInfo[4]):
-        # Initialize the MNIST dataset
-        mnistData = MNISTData()
+    measurementType = getMeasurementTypeFromUser()
 
-        # Initialize the model with the user data
-        print("Initializing Model...")
-        model = intializeModel(userInfo)
+    values = measurementPrompts(measurementType)
 
-        # Compile the model
-        print("Compiling Model...")
-        model = compileModel(model)
+    # # Get the number of hidden layers, neurons, and epochs from user
+    # userInfo = getUserInfo(activFuncSelectionMap)
 
-        # Train the model with the MNIST dataset
-        print("Training Model...")
-        print("Beginning Training with", userInfo[2], "epochs:")
-        startTime = time.perf_counter()
-        model = trainModel(model, mnistData)
-        endTime = time.perf_counter()
-        totalTrainingTime = endTime - startTime
-        print(totalTrainingTime, "seconds.")
-        # TODO: Display training metrics
+    # with tensorFlow.device(userInfo[4]):
+    #     # Initialize the MNIST dataset
+    #     mnistData = MNISTData()
+    #
+    #     # Initialize the model with the user data
+    #     print("Initializing Model...")
+    #     model = intializeModel(userInfo)
+    #
+    #     # Compile the model
+    #     print("Compiling Model...")
+    #     model = compileModel(model)
+    #
+    #     # Train the model with the MNIST dataset
+    #     print("Training Model...")
+    #     print("Beginning Training with", userInfo[2], "epochs:")
+    #     startTime = time.perf_counter()
+    #     model = trainModel(model, mnistData)
+    #     endTime = time.perf_counter()
+    #     totalTrainingTime = endTime - startTime
+    #     print(totalTrainingTime, "seconds.")
+    #     # TODO: Display training metrics
 
 
 def welcomeUser():
@@ -84,10 +103,308 @@ def welcomeUser():
     """
     print("**********************************************************************")
     print()
-    print("Hello! Welcome to Alex Worland's Artificial Neural Network Profiler!")
+    print("Hello! Welcome to Alexandra Worland's Artificial Neural Network Profiler!")
     print()
-    print("This tool profiles a simple ANN that can predict the value of handwritten digits 0-9 using the MNIST dataset.")
+    print(
+        "This tool profiles a simple ANN that can predict the value of handwritten digits 0-9 using the MNIST dataset.")
     print()
+    print("**********************************************************************")
+    print()
+
+
+def getMeasurementTypeFromUser():
+    """
+    A function that gets the measurement type from the user
+    :return: An integer representing the measurement type
+    """
+
+    # TODO: This type of thing happens a lot, maybe refactor it into something that can handle it?
+    # Loop to ensure user input is in the correct format
+    flag = True
+    while flag:
+        print()
+        print("**********************************************************************")
+        while flag:
+            try:
+                print()
+                print("What would you like to measure?")
+                print()
+
+                # List the different measurement types
+                for i in range(len(measurements)):
+                    print(i, ":", measurements.get(i))
+
+                print()
+                measurement = input("Please enter the measurement's number: ")
+                # Cast input to int
+                measurement = int(measurement)
+
+                # Check if the input is within range
+                if measurement >= len(measurements) or measurement < 0:
+                    flag = True
+                    raise ValueError
+                else:
+                    flag = False
+            # If input is invalid, inform user and restart loop
+            except ValueError:
+                inputError(measurement)
+
+        # Loop to ensure user input is in the correct format
+        flag = True
+        while flag:
+            try:
+                print()
+                print("You have selected: \"", measurements.get(measurement), "\"")
+                print()
+                confirmation = input("Is that correct? (y/n): ")
+
+                # Cast input to string
+                confirmation = str(confirmation).lower()
+
+                # Check if input is y or n
+                if confirmation == 'y':
+                    flag = False
+                elif confirmation == 'n':
+                    flag = True
+                else:
+                    # If not y or n, throw a value error
+                    raise ValueError
+                print()
+                print("**********************************************************************")
+                print()
+            except ValueError:
+                inputError(confirmation)
+    return measurement
+
+
+def initializeNetworkList(measurement):
+    print()
+    print("How many networks would you like to measure?")
+
+
+def measurementPrompts(measurement):
+    switch = {
+        0: neuronsVsTime,
+        # 1: hiddenLayersVsTime,
+        # 2: numEpochsVsTime,
+        # 3: neuronsVsAccuracy,
+        # 4: hiddenLayersVsAccuracy,
+        # 5: numEpochsVsAccuracy
+    }
+    func = switch.get(measurement)
+    values = func()
+    return values
+
+
+def neuronsVsTime():
+    layerSizes = []
+    print("**********************************************************************")
+    print()
+    numHiddenLayers = input("How many hidden layers would you like each network to have?")
+    numHiddenLayers = int(numHiddenLayers)
+    print()
+    print("How would you like to alter the number of neurons per hidden layer in each network?")
+    print()
+    for i in range(len(patterns)):
+        print(i, ":", patterns.get(i))
+    print()
+    patternType = input("Please enter the pattern's number: ")
+    patternType = int(patternType)
+    # print()
+    # firstHLayerSize = input("How many neurons would you like your first layer to have? : ")
+    # firstHLayerSize = int(firstHLayerSize)
+    # print()
+    # if patternType == 2:
+    #     layerSizes.append(firstHLayerSize)
+    #     for i in range(1, numHiddenLayers):
+    #         numNeurons = input("How many neurons should network" + str(i + 1) + "/" + str(numHiddenLayers) + "have? : ")
+    #         numNeurons = int(numNeurons)
+    #         layerSizes.append(numNeurons)
+    # else:
+    #     layerSizes = patternFunctions(patternType, numHiddenLayers, firstHLayerSize)
+
+    layerSizes = patternFunctions(patternType, numHiddenLayers)
+
+
+def patternFunctions(patternType, patternSize):
+    switch = {
+        0: linearFunction,
+        1: exponentialFunction,
+        2: polynomialFunction,
+        3: manualEntry
+    }
+    patternFunction = switch.get(patternType)
+    pattern = patternFunction(patternSize)
+    return pattern
+
+
+def linearFunction(patternSize):
+    values = []
+    flag = True
+
+    while flag:
+        while flag:
+            print()
+            print("A linear function will be represented as \"y = mx + b\"")
+            print()
+
+            coefficient = inputPrompt("What should the value of m be?: ", int)
+            print()
+            constant = inputPrompt("What should the value of b be? : ", int)
+            print()
+            variableStart = inputPrompt("What should the starting value of x be? : ", int)
+            print()
+
+            print()
+            print("Function will be: y =", str(coefficient) + "x + " + str(constant))
+            print()
+
+            confirmation = confirmationPrompt()
+            if confirmation:
+                flag = False
+
+        flag = True
+
+        for i in range(patternSize):
+            values.append(coefficient * (variableStart + i) + constant)
+
+        print("Values will be", values)
+
+        confirmation = confirmationPrompt()
+        if confirmation:
+            flag = False
+
+    return values
+
+
+def polynomialFunction(patternSize):
+    values = []
+    flag = True
+    while flag:
+        while flag:
+            print()
+            print("A polynomial function will be represented as \"y = x^n\"")
+            print()
+
+            power = inputPrompt("What should the value of n be?", int)
+            print()
+            variableStart = inputPrompt("What should the starting value of x be?")
+            print()
+
+            print()
+            print("Function will be: y = x^" + str(power))
+            print()
+
+            confirmation = confirmationPrompt()
+            if confirmation:
+                flag = False
+
+        flag = True
+
+        for i in range(patternSize):
+            values.append((variableStart + i) ** power)
+
+        print("Values will be:", values)
+
+        confirmation = confirmationPrompt()
+        if confirmation:
+            flag = False
+
+    return values
+
+
+def exponentialFunction(patternSize):
+    values = []
+    flag = True
+    while flag:
+        while flag:
+            print()
+            print("An exponential function will be represented as \"y = n^x\"")
+            print()
+
+            constant = inputPrompt("What should the value of n be? : ")
+            print()
+            variableStart = inputPrompt("What should the starting value of x be? : ")
+            print()
+
+            print("Function will be: y=", str(constant) + "^x")
+
+            confirmation = confirmationPrompt()
+            if confirmation:
+                flag = False
+
+        flag = True
+
+        for i in range(patternSize):
+            values.append(constant ** (variableStart + i))
+
+        print("Values will be:", values)
+
+        confirmation = confirmationPrompt()
+        if confirmation:
+            flag = False
+
+    return values
+
+
+def manualEntry(patternSize):
+    values = []
+    # TODO: Add a way to manually change values without having to start over
+    flag = True
+    while flag:
+        for i in range(patternSize):
+            value = inputPrompt("Please enter a value for x = " + str(i), int)
+            values.append(value)
+        print("Values will be:", values)
+        confirmation = confirmationPrompt()
+        if confirmation:
+            flag = False
+    return values
+
+
+def inputPrompt(message, typeVar):
+    flag = True
+    value = None
+    while flag:
+        try:
+            value = input(message)
+            value = typeVar(value)
+            flag = False
+        except ValueError:
+            inputError(value)
+        except TypeError:
+            inputError(value)
+    return value
+
+
+def confirmationPrompt():
+    flag = True
+    message = "Is that ok? (y/n) : "
+    while flag:
+        choice = inputPrompt(message, str).lower()
+        if choice == 'y':
+            return True
+        elif choice == 'n':
+            return False
+        else:
+            inputError(choice)
+
+
+# def hiddenLayersVsTime():
+# def numEpochsVsTime():
+# def neuronsVsAccuracy():
+# def hiddenLayersVsAccuracy():
+# def numEpochsVsAccuracy():
+
+def inputError(thrownValue):
+    """
+    A function that informs the user that their input was invalid
+    :param thrownValue: The invalid input
+    :return:
+    """
+    print()
+    print("**********************************************************************")
+    print("Option: \"", thrownValue, "\" is invalid. Please enter a valid option.")
     print("**********************************************************************")
     print()
 
@@ -101,30 +418,30 @@ def getUserInfo(activationFunctionMap):
     flag = True
     while flag:
         try:
-            numHiddenLayers = int(input("How many hidden layers would you like the network to have? : "))
+            numHiddenLayers = int(input("How many hidden layers would you like the network to have?:"))
             print()
-            numNeurons = int(input("How many neurons would you like those layers to have? : "))
+            numNeurons = int(input("How many neurons would you like those layers to have?:"))
             print()
-            numEpochs = int(input("How many epochs would you like to train the network? : "))
+            numEpochs = int(input("How many epochs would you like to train the network?:"))
             print()
-            print("******************************************************************")
+            print("**********************************************************************")
             print()
             print("What activation function would you like to use for those neurons?")
             print()
             for i in range(len(activationFunctionMap)):
-                print(i, " : ", activationFunctionMap.get(i))
+                print(i, ":", activationFunctionMap.get(i))
             print()
             activationFunction = int(input("Please enter the activation function's number: "))
             print()
-            print("******************************************************************")
+            print("**********************************************************************")
             print()
             print("What device would you like to train this network on?")
             devices = device_lib.list_local_devices()
             for i in range(len(devices)):
-                print(i, " : ", devices[i])
+                print(i, ":", devices[i])
             device = int(input("Please enter the device you would like to train the network with: "))
             print()
-            print("******************************************************************")
+            print("**********************************************************************")
             print()
             device = devices[device].name
             flag = False
@@ -134,7 +451,7 @@ def getUserInfo(activationFunctionMap):
     return [numHiddenLayers, numNeurons, numEpochs, activationFunction, device]
 
 
-def intializeModel(userInfo):
+def initializeModel(userInfo):
     """
     A function that initializes the model based on the user input
     :param userInfo: an array containing relevant information from the user
